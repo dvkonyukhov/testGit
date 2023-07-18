cf8b1e4b42f24b7ca93ba09e97d8068464b52bfd
d.konyuhov <w1356@yandex.ru>
2023-07-18 11:05:40 +0300

    1

--- src/main/java/ru/dkonyukhov/bootTest/Main.java
+++ src/main/java/ru/dkonyukhov/bootTest/Main.java
@@ -2,8 +2,8 @@ package main.java.ru.dkonyukhov.bootTest;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("test1");
         Class1.say();
+        System.out.println("1");
     }
 }
 
