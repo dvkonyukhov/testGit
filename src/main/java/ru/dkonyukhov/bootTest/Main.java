08e99b1890b41143614a2c1d0a11182e6f6714d8
d.konyuhov <w1356@yandex.ru>
2023-07-18 11:27:48 +0300

    2

--- src/main/java/ru/dkonyukhov/bootTest/Main.java
+++ src/main/java/ru/dkonyukhov/bootTest/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Class1.say();
         System.out.println("1");
+        System.out.println("2");
+        System.out.println("psw");
     }
 }
 
